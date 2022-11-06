66455d3217de1f5e1be7ddd1b23c4f240e8d034e
Rogers Mezones <rogers.mezones@gmail.com>
2022-11-06 01:10:31 -0300

    funcionando y mostrando los nombres de la DB, falta arreglar rutas y arrglar html para mejor presentacion

--- MVT_Coder/Blog_coder/views.py
+++ MVT_Coder/Blog_coder/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from Blog_coder.models import Familiares
+from django.template import loader
+from django.http import HttpResponse
+
+
 
 # Create your views here.
 
 def Listado_Familiares(request):
-    return render(request, "Blog_coder/index.html")
+    
+    lista_de_Familiares = Familiares.objects.all()
+    
+        
+    familia = { "lista_familiares": lista_de_Familiares}
+
+    plantilla = loader.get_template("Blog_coder/index.html")
+
+    documento = plantilla.render(familia)
+    #return render(request, "Blog_coder/index.html")
+
+    return HttpResponse(documento)
